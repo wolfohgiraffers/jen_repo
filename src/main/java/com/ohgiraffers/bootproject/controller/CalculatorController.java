c098efbb27f019e0dc994d06301d6e069bea5d08
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-11-10 09:11:07 +0900

    good monday

--- src/main/java/com/ohgiraffers/bootproject/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/bootproject/controller/CalculatorController.java
@@ -25,7 +25,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm Alive! Good Weather! Good Jenkins!!!";
+        return "I'm Alive! Good Weather! Good Monday!!!";
     }
 
 //    @GetMapping("/plus")
